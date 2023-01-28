58abcbac45c91229d2fed777740cf8f81b810e90
aonyshchuk <alinaonyshchuk7@gmail.com>
2023-01-28 12:19:33 +0300

    init commitAdd project files.

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+﻿/*Завдання 1. Напишіть програму, яка обчислює суму цілих 
+чисел від а до 500 (значення a вводиться з клавіатури).
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int start;
+	int end = 500; 
+	int sum = 0; 
+
+	cout << "Start =";
+	cin >> start;
+
+	for (int i = start; i <= end; ++i)
+		sum += i; 
+
+	cout << "Sum = " << sum << endl; 
+	
+	
+	return 0;
+}
