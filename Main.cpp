fb3e2a6375da1972ae5fdc01845305b337e83d26
aonyshchuk <alinaonyshchuk7@gmail.com>
2023-01-28 12:19:33 +0300

    Done task

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+﻿/* Завдання 3. Користувач вводить будь-яке ціле число. 
+Необхідно з цього цілого числа видалити всі цифри 3 і 6 
+і вивести назад на екран.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter number -> ";
+	cin >> n;
+
+    while (n > 0 && (n % 10 == 3 || n % 10 == 6))
+        n /= 10;
+
+    int deln36 = n % 10;
+    int i = 10;
+    while (n > 0) {
+        
+        n /= 10;
+        if (n % 10 != 3 && n % 10 != 6) {
+      
+            deln36 += (n % 10) * i;
+            i *= 10;
+        }
+    }
+
+    cout << deln36 << endl;
+	return 0;
+}
