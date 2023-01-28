b7ac6c746305ba0b238d7d6809b814e23ce769fa
aonyshchuk <alinaonyshchuk7@gmail.com>
2023-01-28 12:19:33 +0300

    doneAdd project files.

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+﻿/* Завдання 6. Користувач вводить ціле число. Необхідно 
+вивести всі цілі числа, на які задане число ділиться без 
+залишку. 
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Enter a -> ";
+	cin >> a;
+
+	for (int i = 1; i < a; i++)
+	{
+		if (a % i == 0) {
+			cout << i << endl;
+		}
+	}
+	return 0;
+}
