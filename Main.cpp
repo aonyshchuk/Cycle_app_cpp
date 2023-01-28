645594a5c1a6d1c50f177e71c8e4e477820e90ef
aonyshchuk <alinaonyshchuk7@gmail.com>
2023-01-28 12:19:33 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+﻿/*Завдання 2. Напишіть програму, яка запитує два цілих 
+числа x і y, після чого обчислює і виводить значення x у 
+степені y.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 0;
+	int y = 0;
+	int pow{}; {};
+
+	cout << "Enter x =";
+	cin >> x;
+	cout << "Enter y =";
+	cin >> y;
+
+	while (y >= 0)
+
+	{
+		cout << "Result = " << pow = x ^ y << endl;
+		cout << endl; 
+	}
+	
+	
+	return 0;
+}
